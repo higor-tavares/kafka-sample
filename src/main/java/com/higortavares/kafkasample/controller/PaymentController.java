0465d9e087d451399baaeb1592d19ba5c9827fbf
paulo.tavares <paulo.tavares@olxbr.com>
2023-09-09 14:15:33 -0300

    Add sychronized producer to payments and a simple producer to log messages

--- src/main/java/com/higortavares/kafkasample/controller/PaymentController.java
+++ src/main/java/com/higortavares/kafkasample/controller/PaymentController.java
@@ -3,7 +3,9 @@ package com.higortavares.kafkasample.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.higortavares.kafkasample.controller.request.PaymentRequest;
+import com.higortavares.kafkasample.infraestructure.LogProducer;
 import com.higortavares.kafkasample.infraestructure.PaymentKafkaProducer;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PaymentController {
   private final PaymentKafkaProducer producer;
+  private final LogProducer logs;
   private final ObjectMapper objectMapper;
   @PostMapping("/")
   public ResponseEntity<PaymentRequest> pay(@RequestBody PaymentRequest paymentRequest) throws JsonProcessingException {
+    logs.sendMessage(UUID.randomUUID().toString(), String.format("New payments order ID [%s]. amount [%d]...", paymentRequest.getPaymentID(), paymentRequest.getAmount()));
     producer.sendMessage(paymentRequest.getPaymentID(), objectMapper.writeValueAsString(paymentRequest));
+    logs.sendMessage(UUID.randomUUID().toString(), String.format("Payments order received with success [%s]...", paymentRequest.getPaymentID()));
     return ResponseEntity.ok(paymentRequest);
   }
 }
--- src/main/java/com/higortavares/kafkasample/infraestructure/LogProducer.java
+++ src/main/java/com/higortavares/kafkasample/infraestructure/LogProducer.java
@@ -8,16 +8,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class PaymentKafkaProducer {
-  @Value("${payment.topic}")
+public class LogProducer {
+  @Value("${log.topic}")
   private String topic;
   private final KafkaProducer<String, String> producer;
-
   public void sendMessage(String key, String message) {
     ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, message);
     try {
-      //send the message synchronously
-      this.producer.send(record).get();
+      /*
+       * The send() method returns a Java Future object with RecordMetadata,
+       * but since we simply ignore the returned value,
+       * we have no way of knowing whether the mes‐ sage was sent successfully or not.
+       * This method of sending messages can be used when dropping a message silently is acceptable.
+       */
+      this.producer.send(record);
     } catch (Exception e) {
       e.printStackTrace();
     }
